45a07a26f5128e51dcd772e2174a4d0d0e371af3
Sampath <sampath.skyalliances@gmail.com>
2019-08-14 18:56:24 +0530

    Some tests added

--- src/model/NumberModel.php
+++ src/model/NumberModel.php
@@ -8,8 +8,16 @@ namespace SriAnt\Utils;
  * Description      :   Contains essential functions for validate and convert number to readable format.
  */
 
-class NumberModel{
-    public function validateNumber($number){
-
+class NumberModel
+{
+    public function validateNumber($number)
+    {
+        if ($number != null) {
+            $number = trim($number);
+            if (strlen($number) > 0) {
+                return is_numeric($number);
+            }
+        }
+        return false;
     }
 }
--- test/NumberTest.php
+++ test/NumberTest.php
@@ -14,10 +14,21 @@
 
  final class NumberTest extends TestCase
  {
+
      public function testNumber(): void
      {
        $number_model = new NumberModel();
        $this->assertInstanceOf(NumberModel::class, $number_model);
+     }
+
+     public function testVeriry(){
+       $number_model = new NumberModel();
+       $this->assertFalse($number_model->validateNumber(null));
+       $this->assertFalse($number_model->validateNumber(""));
+       $this->assertTrue($number_model->validateNumber(1));
+       $this->assertTrue($number_model->validateNumber(1.2));
+       $this->assertTrue($number_model->validateNumber(100.22));
+       $this->assertTrue($number_model->validateNumber(9999999.999999));
      }
 
 
