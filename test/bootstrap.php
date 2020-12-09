2b224859ddd9b660290418ef656bc29c199f5bad
Sampath Abeysinghe <67.sampath@gmail.com>
2020-12-09 11:07:43 +0530

    bootstrap file added. NBProject configured.

---
+++ test/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author Sampath Abeysinghe
+ */
+// TODO: check include path
+//ini_set('include_path', ini_get('include_path'));
+require dirname(__DIR__).'/vendor/autoload.php';
+// put your code here
